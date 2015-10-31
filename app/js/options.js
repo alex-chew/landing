1a3316d86973103318ddb28f15df2ce6f65fa6d3
Alex Chew <alex@alexchew.net>
2015-10-31 01:50:37 -0700

    Complete options page

--- app/js/options.js
+++ app/js/options.js
@@ -2,7 +2,6 @@ document.body.onload = function() {
   chrome.storage.sync.get("landing_prefs", function(items) {
     if (!chrome.runtime.error && items.landing_prefs) {
       var prefs = items.landing_prefs;
-      console.log(prefs);
 
       var schema = {
         "title": "Categories",
@@ -32,9 +31,29 @@ document.body.onload = function() {
       var edCon = document.getElementsByClassName("editor-container")[0];
       var editor = new JSONEditor(edCon, {
         schema: schema,
+        startval: prefs.categories,
+        disable_collapse: true,
+        required_by_default: true,
         theme: "bootstrap3",
         iconlib: "bootstrap3"
       });
+
+      var saveButton = document.getElementsByClassName("editor-save")[0];
+      saveButton.onclick = function() {
+        var errors = editor.validate();
+        if (errors.length) {
+          alert("Validation errors! See console for details.");
+          console.log(errors);
+        } else {
+          chrome.storage.sync.set({
+            "landing_prefs": {categories: editor.getValue()}
+          }, function() {
+            if (chrome.runtime.error) {
+              console.log("Runtime error");
+            }
+          });
+        }
+      };
     }
   });
 };
