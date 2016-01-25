f2d2b0099784bf7ecfd2100b341bd781671c1a06
Alex Chew <alex@alexchew.net>
2016-01-24 22:31:22 -0800

    Add working options page that interacts with storage

--- app/js/options.js
+++ app/js/options.js
@@ -1,60 +1,49 @@
 document.body.onload = function() {
-  chrome.storage.sync.get("landing_prefs", function(items) {
-    if (!chrome.runtime.error && items.landing_prefs) {
-      var prefs = items.landing_prefs;
-
-      var schema = {
-        "title": "Categories",
-        "type": "array",
-        "format": "table",
-        "items":{
-          "type": "object",
-          "title": "Category",
-          "properties": {
-            "name": {"type": "string"},
-            "links":{
-              "type": "array",
-              "format": "table",
-              "items": {
-                "type": "object",
-                "title": "Link",
-                "properties": {
-                  "name": {"type": "string"},
-                  "url": {"type": "string"}
-                }
-              }
-            }
-          }
-        }
-      };
-
-      var edCon = document.getElementsByClassName("editor-container")[0];
-      var editor = new JSONEditor(edCon, {
-        schema: schema,
-        startval: prefs.categories,
-        disable_collapse: true,
-        required_by_default: true,
-        theme: "bootstrap3",
-        iconlib: "bootstrap3"
-      });
-
-      var saveButton = document.getElementsByClassName("editor-save")[0];
-      saveButton.onclick = function() {
-        var errors = editor.validate();
-        if (errors.length) {
-          alert("Validation errors! See console for details.");
-          console.log(errors);
-        } else {
-          chrome.storage.sync.set({
-            "landing_prefs": {categories: editor.getValue()}
-          }, function() {
-            if (chrome.runtime.error) {
-              console.log("Runtime error");
-            }
-          });
-        }
-      };
+  chrome.storage.sync.get("landing_options", function(items) {
+    if (chrome.runtime.error) {
+      console.log("Could not get options from Chrome storage");
+      return;
     }
+
+    // If no options exist yet, reset to defaults
+    if (!items.landing_options) {
+      setToDefaults();
+    }
+
+    populateInputs(items.landing_options);
   });
+
+  var saveButton = document.getElementsByName("save")[0];
+  saveButton.addEventListener("click", saveOptions);
 };
+
+function populateInputs(options) {
+  chrome.storage.sync.get("landing_options", function(items) {
+    // Populate inputs with options
+    document.getElementsByName("bgcolor")[0].value = options.bgcolor;
+    document.getElementsByName("lncolor")[0].value = options.lncolor;
+    document.getElementsByName("txcolor")[0].value = options.txcolor;
+  });
+}
+
+function saveOptions() {
+  chrome.storage.sync.set({
+    landing_options: {
+      bgcolor: document.getElementsByName("bgcolor")[0].value,
+      lncolor: document.getElementsByName("lncolor")[0].value,
+      txcolor: document.getElementsByName("txcolor")[0].value
+    }
+  }, function() {});
+}
+
+function setToDefaults() {
+  console.log("Setting defaults");
+  chrome.storage.sync.set({
+    landing_options: {
+      bgcolor: "#2b303b", // navy blue
+      lncolor: "#4f5b66", // light navy
+      txcolor: "#eff1f5" // bright bluish white
+    }
+  }, function() {});
+}
 
