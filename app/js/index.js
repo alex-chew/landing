961b4646e3ff898fa381c66b5c553aec9286d6d2
Alex Chew <alex@alexchew.net>
2016-01-24 18:30:32 -0800

    Transition from using custom model to bookmarks API

--- app/js/index.js
+++ app/js/index.js
@@ -1,43 +1,72 @@
-document.body.onload = function() {
-  chrome.storage.sync.get("landing_prefs", function(items) {
-    if (!chrome.runtime.error && items.landing_prefs) {
-      var categories = items.landing_prefs.categories;
+function createIndex(baseFolder) {
+  if (!baseFolder) {
+    console.log("Base folder not found");
+    return;
+  }
 
-      var content = document.getElementsByClassName("list")[0];
-      categories.forEach(function(cat, idx, arr) {
-        var row = document.createElement("tr");
-        row.setAttribute("class", "list__row");
+  // Populate index page
+  chrome.bookmarks.getSubTree(baseFolder.id, function(tree) {
+    // Get array of category (folder) nodes
+    var categories = tree[0].children;
 
-        var catCell = document.createElement("td");
-        catCell.setAttribute("class", "list__category");
-        catCell.innerHTML = cat.name;
+    // Main list element on page (from index.html)
+    var content = document.getElementsByClassName("list")[0];
 
-        var itemsCell = document.createElement("td");
-        itemsCell.setAttribute("class", "list__items");
+    // Populate each category row
+    categories.forEach(function(cat, idx, arr) {
+      // Create table row
+      var row = document.createElement("tr");
+      row.setAttribute("class", "list__row");
 
-        var itemsList = document.createElement("ul");
-        itemsList.setAttribute("class", "list__items-list");
+      // Create cell with category title
+      var catCell = document.createElement("td");
+      catCell.setAttribute("class", "list__category");
+      catCell.innerHTML = cat.title;
 
-        cat.links.forEach(function(ln, idx, arr) {
-          var item = document.createElement("li");
-          item.setAttribute("class", "list__items-list__item");
+      // Create cell for links, and list to hold the links
+      var itemsCell = document.createElement("td");
+      itemsCell.setAttribute("class", "list__items");
+      var itemsList = document.createElement("ul");
+      itemsList.setAttribute("class", "list__items-list");
 
-          var link = document.createElement("a");
-          link.setAttribute("href", ln.url);
-          link.setAttribute("class", "list__items-list__link");
-          link.innerHTML = ln.name;
+      // Populate category link lists
+      cat.children.forEach(function(bookmark) {
+        // Create list item
+        var item = document.createElement("li");
+        item.setAttribute("class", "list__items-list__item");
 
-          item.appendChild(link);
-          itemsList.appendChild(item);
-        });
+        // Create link to place in link
+        var link = document.createElement("a");
+        link.setAttribute("href", bookmark.url);
+        link.setAttribute("class", "list__items-list__link");
+        link.innerHTML = bookmark.title;
 
-        itemsCell.appendChild(itemsList);
-
-        row.appendChild(catCell);
-        row.appendChild(itemsCell);
-        content.appendChild(row);
+        // Add link element to item, and add item to list
+        item.appendChild(link);
+        itemsList.appendChild(item);
       });
-    }
+
+      // Add everything to the row, and add the row to the content element
+      itemsCell.appendChild(itemsList);
+      row.appendChild(catCell);
+      row.appendChild(itemsCell);
+      content.appendChild(row);
+    });
+  });
+}
+
+document.body.onload = function() {
+  chrome.bookmarks.get("2", function(nodes) { // id "2" is "Other bookmarks"
+    var baseFolder;
+
+    // Iterate over "Other bookmarks" to find base folder
+    chrome.bookmarks.getChildren(nodes[0].id, function(nodes) {
+      baseFolder = nodes.find(function(node, idx, arr) {
+        return node.title == "landing";
+      });
+
+      createIndex(baseFolder);
+    });
   });
 };
 
