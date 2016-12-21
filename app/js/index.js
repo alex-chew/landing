82598287759a3e780974a18e3b0d20704e043d67
Alex Chew <alex@alexchew.net>
2016-12-20 23:36:36 -0800

    Simplify CSS classes

--- app/js/index.js
+++ app/js/index.js
@@ -30,36 +30,35 @@ function createIndex(baseFolder) {
 
     // Add main table to container
     var content = document.createElement("table");
-    content.setAttribute("class", "list");
+    content.setAttribute("class", "table");
     container.appendChild(content);
 
     // Populate each category row
     categories.forEach(function(cat, idx, arr) {
       // Create table row
       var row = document.createElement("tr");
-      row.setAttribute("class", "list__row");
+      row.setAttribute("class", "row");
 
       // Create cell with category title
       var catCell = document.createElement("td");
-      catCell.setAttribute("class", "list__category text-custom");
+      catCell.setAttribute("class", "category text-custom");
       catCell.innerHTML = cat.title;
 
       // Create cell for links, and list to hold the links
       var itemsCell = document.createElement("td");
-      itemsCell.setAttribute("class", "list__items");
       var itemsList = document.createElement("ul");
-      itemsList.setAttribute("class", "list__items-list line-custom");
+      itemsList.setAttribute("class", "list line-custom");
 
       // Populate category link lists
       cat.children.forEach(function(bookmark) {
         // Create list item
         var item = document.createElement("li");
-        item.setAttribute("class", "list__items-list__item");
+        item.setAttribute("class", "item");
 
         // Create link to place in link
         var link = document.createElement("a");
         link.setAttribute("href", bookmark.url);
-        link.setAttribute("class", "list__items-list__link text-custom");
+        link.setAttribute("class", "link text-custom");
         link.innerHTML = bookmark.title;
 
         // Add link element to item, and add item to list
