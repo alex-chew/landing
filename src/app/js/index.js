593bb86d47714aa483d2a56a4c417d949dc6b873
Alex Chew <alex@alexchew.net>
2017-03-01 15:41:57 -0800

    Simplify createDom using template literals

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -1,58 +1,22 @@
 (() => {
   var createDom = function(categories, options) {
-    // Main container on page (from index.html)
-    var container = document.getElementsByClassName("container")[0];
-
-    // Add main table to container
-    var content = document.createElement("table");
-    content.setAttribute("class", "table text-custom");
-    container.appendChild(content);
-
-    // Populate each category row
-    categories.forEach(function(cat, idx, arr) {
-      // Create table row
-      var row = document.createElement("tr");
-      row.setAttribute("class", "row");
-
-      // Create cell with category title
-      var catCell = document.createElement("td");
-      catCell.setAttribute("class", "category");
-      catCell.innerHTML = cat.title;
-
-      // Create cell for links, and list to hold the links
-      var itemsCell = document.createElement("td");
-      var itemsList = document.createElement("ul");
-      itemsList.setAttribute("class", "list line-custom");
-
-      // Populate category link lists
-      cat.children.forEach(function(bookmark) {
-        // Create list item
-        var item = document.createElement("li");
-        item.setAttribute("class", "item");
-
-        // Create link to place in link
-        var link = document.createElement("a");
-        link.setAttribute("href", bookmark.url);
-        link.setAttribute("class", "link");
-        link.innerHTML = bookmark.title;
-
-        // Add link element to item, and add item to list
-        item.appendChild(link);
-        itemsList.appendChild(item);
-      });
-
-      // Add everything to the row, and add the row to the content element
-      itemsCell.appendChild(itemsList);
-      row.appendChild(catCell);
-      row.appendChild(itemsCell);
-      content.appendChild(row);
-    });
+    // Create rows with title and links
+    var bookmarks = document.getElementById("bookmarks");
+    bookmarks.innerHTML = categories.map(category => {
+      var items = category.children
+        .map(bookmark => `<li class="item">
+            <a class="link" href="${bookmark.url}">${bookmark.title}</a>
+            </li>`)
+        .join("");
+      return `<tr class="row">
+        <td class="category">${category.title}</td>
+        <td><ul class="list line-custom">${items}</ul></td>
+        </tr>`;
+    }).join("");
 
     // Add quote
-    var quote = document.createElement("div");
-    quote.setAttribute("class", "quote text-custom");
+    var quote = document.getElementById("quote");
     quote.innerHTML = options.quote;
-    container.appendChild(quote);
   };
 
   var setStyles = function(options) {
